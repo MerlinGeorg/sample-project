dc9853bc181b20a5c75d97b800bb015e7ca7aa22
MerlinGeorg <merlingeo96@gmail.com>
2022-11-11 14:00:44 +0530

    vue project

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div :style="{fontSize:postFontSize+'em'}" >
+  <BlogPost v-for="post in posts" :key="post.id" :title="post.title" @enlarge-text="postFontSize+=1"></BlogPost>
+</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import BlogPost from './components/BlogPost.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    BlogPost
+},
+  data(){
+    return{
+      posts:[
+        {id:1,title:'My journey with Vue'},
+        {id:2,title:'Blogging with Vue'},
+        {id:3,title:'Why Vue is so fun'}
+      ],
+      postFontSize:1
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/BlogPost.vue
@@ -0,0 +1,12 @@
+<!-- props -->
+<script>
+    export default{
+        props:['title'],
+      //  emits:['enlarge-text']
+    }
+</script>
+
+<template>
+    <h4>{{title}}</h4>
+    <button @click="$emit('enlarge-text')">Enlarge Text</button>
+</template>
